4b7ad21396909a4f728fee71a038eea07055312b
kurosu66 <kurosu66@gmail.com>
2026-04-26 22:23:51 +0900

    xIsNextリファクタ

--- src/App.js
+++ src/App.js
@@ -53,8 +53,8 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const xIsNext = currentMove % 2 === 0;
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
 
